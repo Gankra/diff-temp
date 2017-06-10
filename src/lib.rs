13453b2cca63990aeae00a3be17da3702f9a2ef1
Alexis Beingessner <a.beingessner@gmail.com>
2017-06-10 14:47:56 -0400

    acl

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<'a, T: ?Sized> !Send for MutexGuard<'a, T> { }
+#[stable(feature = "mutexguard", since = "1.18.0")]
+unsafe impl<'a, T: ?Sized + Sync> Sync for MutexGuard<'a, T> { }
